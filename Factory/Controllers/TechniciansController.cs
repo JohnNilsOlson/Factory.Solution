70f72de425133d6e2056bccb86ddc57af5e70a90
JohnNils Olson <johnnils@gmail.com>
2020-08-07 14:05:52 -0700

    add deletemachine route

--- Factory/Controllers/TechniciansController.cs
+++ Factory/Controllers/TechniciansController.cs
@@ -90,5 +90,22 @@ namespace FactoryControllers
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = technician.TechnicianId });
     }
+
+    public ActionResult DeleteMachine(int id)
+    {
+      var joinEntry = _db.Qualifications.FirstOrDefault(entry => entry.QualificationId == id);
+      ViewBag.Technician = _db.Technicians.FirstOrDefault(technician => technician.TechnicianId == joinEntry.TechnicianId);
+      ViewBag.Machine = _db.Machines.FirstOrDefault(machine => machine.MachineId == joinEntry.MachineId);
+      return View(joinEntry);
+    }
+
+    [HttpPost, ActionName("DeleteMachine")]
+    public ActionResult DeleteMachineConfirmed(int id)
+    {
+      var joinEntry = _db.Qualifications.FirstOrDefault(entry => entry.QualificationId == id);
+      _db.Qualifications.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.TechnicianId });
+    }
   }
 }
