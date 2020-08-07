b3e78fda5ca89772238f5fc5318f5b9c3c56dabf
JohnNils Olson <johnnils@gmail.com>
2020-08-07 12:50:53 -0700

    add addtechnician route

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -1,6 +1,7 @@
 using Factory.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -37,6 +38,8 @@ namespace Factory.Controllers
     public ActionResult Details(int id)
     {
       var thisMachine = _db.Machines
+        .Include(machine => machine.Technicians)
+        .ThenInclude(join => join.Technician)
         .FirstOrDefault(machine => machine.MachineId == id);
       return View(thisMachine);
     }
@@ -70,5 +73,22 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult AddTechnician(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      ViewBag.TechnicianId = new SelectList(_db.Technicians, "TechnicianId", "TechnicianName");
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult AddTechnician(Machine machine, int TechnicianId)
+    {
+      if (TechnicianId != 0)
+      {
+        _db.Qualifications.Add(new Qualification() { TechnicianId = TechnicianId, MachineId = machine.MachineId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = machine.MachineId });
+    }
   }
 }
