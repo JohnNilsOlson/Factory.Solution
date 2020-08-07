5f17ba5295b86ce3487d5008b78716266212975b
JohnNils Olson <johnnils@gmail.com>
2020-08-07 08:54:08 -0700

    add machine class

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.Technicians = new HashSet<Qualification>();
+    }
+    public int MachineId { get; set; }
+    public string MachineName { get; set; }
+
+    public virtual ICollection<Qualifacation> Technicians { get; set; }
+  }
+}
