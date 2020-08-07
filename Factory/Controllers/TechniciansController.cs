f6f3c2bb8cc502333dfebfc51b1276354093560f
JohnNils Olson <johnnils@gmail.com>
2020-08-07 11:12:17 -0700

    add create route

--- Factory/Controllers/TechniciansController.cs
+++ Factory/Controllers/TechniciansController.cs
@@ -20,5 +20,18 @@ namespace FactoryControllers
       List<Technician> model = _db.Technicians.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Technician technician)
+    {
+      _db.Technicians.Add(technician);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
