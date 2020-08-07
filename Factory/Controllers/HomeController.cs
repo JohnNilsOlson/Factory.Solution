5bf9240018a499b07cfa4176b88a316087087b9a
JohnNils Olson <johnnils@gmail.com>
2020-08-07 15:24:15 -0700

    add viewbags to index route

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,11 +1,25 @@
+using Factory.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Contollers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     public ActionResult Index()
     {
+      ViewBag.Machines = _db.Machines.ToList();
+      ViewBag.Technicians = _db.Technicians.ToList();
       return View();
     }
   }
